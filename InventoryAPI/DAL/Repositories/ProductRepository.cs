9ca793dc1e57238bccd4f60589b12f6d8098eb9f
florencia dagotto <flopydagotto@gmail.com>
2025-07-09 22:07:19 -0300

    add repository

---
+++ InventoryAPI/DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace DAL.Repositories
+{
+    public interface IProductRepository
+    {
+
+    }
+
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductsDbContext _context;
+
+        public ProductRepository(ProductsDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
