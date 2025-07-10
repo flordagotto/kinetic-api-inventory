31428ef9f5cbf5f3c61f3774ec4b40bbe80fdf04
florencia dagotto <flopydagotto@gmail.com>
2025-07-09 22:01:23 -0300

    add dto and dal layers

---
+++ InventoryAPI/DAL/DI/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DAL.DI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ProductsDbContext>(options =>
+                options.UseSqlite("Data Source=notifications.db"));
+
+            return services;
+        }
+    }
+}
---
+++ InventoryAPI/DTOs/ProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace DTOs
+{
+    public class ProductDTO
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Category { get; set; } = null!;
+    }
+}
