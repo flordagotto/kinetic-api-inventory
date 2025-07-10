334c4295eb4ce1b660bf1a37b442a8a809880a80
florencia dagotto <flopydagotto@gmail.com>
2025-07-10 10:57:46 -0300

    add delete method in service

--- InventoryAPI/Services/Services/ProductService.cs
+++ InventoryAPI/Services/Services/ProductService.cs
@@ -3,6 +3,7 @@ using DAL.Entities;
 using DAL.Repositories;
 using DTOs;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
 
 namespace Services.Services
 {
@@ -99,6 +100,26 @@ namespace Services.Services
             catch (Exception ex)
             {
                 _logger.LogError(ex.Message, "Error updating product.");
+                throw;
+            }
+        }
+
+        public async Task Delete(Guid id)
+        {
+            try
+            {
+                var product = await _productRepository.GetById(id);
+
+                if (product == null)
+                    throw new ArgumentException($"Product with id {id} not found");
+                // TODO: podria usar excepciones personalizadas y un middleware que trate esta excepcion como un 400
+
+                if (product != null)
+                    await _productRepository.Delete(product);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, "Error deleting product.");
                 throw;
             }
         }
