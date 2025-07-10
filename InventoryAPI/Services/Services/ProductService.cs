1960f316d99e3a31addf788fd6ee4964a028a69c
florencia dagotto <flopydagotto@gmail.com>
2025-07-10 00:17:01 -0300

    add create method in service and mapper

--- InventoryAPI/Services/Services/ProductService.cs
+++ InventoryAPI/Services/Services/ProductService.cs
@@ -1,19 +1,44 @@
-﻿using DAL.Repositories;
+﻿using AutoMapper;
+using DAL.Entities;
+using DAL.Repositories;
+using DTOs;
+using Microsoft.Extensions.Logging;
 
 namespace Services.Services
 {
     public interface IProductService
     {
-
+        Task Create(NewProductDTO newProductDTO);
     }
 
     public class ProductService : IProductService
     {
         private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<ProductService> _logger;
 
-        public ProductService(IProductRepository productRepository)
+        public ProductService(IProductRepository productRepository, IMapper mapper, ILogger<ProductService> logger)
         {
             _productRepository = productRepository;
+            _mapper = mapper;
+            _logger = logger;
         }
+
+
+        public async Task Create(NewProductDTO newProductDTO)
+        {
+            try
+            {
+                var product = _mapper.Map<Product>(newProductDTO);
+
+                await _productRepository.Add(product);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, "Error creating a player.");
+                throw;
+            }
+        }
+
     }
 }
